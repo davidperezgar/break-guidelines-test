39488deefa83d2cb7b34e6cd8b38a030a556c223
davidperezgar <david@closemarketing.es>
2024-09-14 12:25:17 +0200

    break translations

---
+++ break-translation-functions.php
@@ -0,0 +1,17 @@
+<?php
+
+$translation_slug = 'break-guidelines-test';
+
+echo esc_html__( 'Hello, world!' );
+
+echo esc_html__( 'Hello, world!', $translation_slug );
+
+echo sprintf(
+	esc_html__( 'Hello, %s!', 'world' ),
+	'world'
+);
+
+echo sprintf(
+	esc_html__( 'Hello, %s!', $translation_slug ),
+	'world'
+);
