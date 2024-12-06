33b2edb88e13e2b2306bd7d559ba337e58e41ab9
davidperezgar <david@closemarketing.es>
2024-12-06 10:50:56 +0100

    break images

---
+++ break-images.php
@@ -0,0 +1,3 @@
+<div><img src="<?php echo plugins_url( 'img/logo.png', __FILE__ ) ?>" alt="Logo" class="pe-3" /></div>
+
+<img src="<?php echo esc_url( plugins_url( 'img/logo.png', __FILE__ ) ); ?>" ... />
